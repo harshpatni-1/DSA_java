295ca6949ca45f0ed53d38d337afe62c1fb3d39e
harshpatni-1 <harshpatni81@gmail.com>
2026-01-09 19:16:23 +0000

    done

--- Main.java
+++ Main.java
@@ -1,36 +1,14 @@
-# A simple beginner-friendly Python program: Guess the Number Game
+import java.util.Scanner;
 
-# This program picks a random number between 1 and 100,
-# and asks the player to guess it. It gives hints if the guess is too high or too low.
-
-import random  # We need this to generate a random number
-
-# Generate a random secret number between 1 and 100
-secret_number = random.randint(1, 100)
-
-print("Welcome to the Guess the Number game!")
-print("I'm thinking of a number between 1 and 100.")
-print("Try to guess it!")
-
-# Keep track of how many guesses the player makes
-guesses = 0
-
-# The game loop - continues until the player guesses correctly
-while True:
-    # Ask the player for a guess
-    guess = int(input("Enter your guess: "))
+Public static void main(String[]args){
+    Scanner = new Scanner(System.in);
+    int number = Scanner.next
     
-    # Count this guess
-    guesses += 1
-    
-    # Check if the guess is correct
-    if guess == secret_number:
-        print(f"Congratulations! You got it! The number was {secret_number}.")
-        print(f"It took you {guesses} guesses.")
-        break  # Exit the loop because the game is over
-    elif guess < secret_number:
-        print("Too low! Try a higher number.")
-    else:
-        print("Too high! Try a lower number.")
+}
 
-print("Thanks for playing!")
+   if number % 2 
+   else 
+   if number % 3
+   else
+   if number % 5
+ System.out.println("the number is prime number ")
