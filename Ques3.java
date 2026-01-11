eda3fd53463afd367fc7cbcca5bc6571bcfae336
harshpatni-1 <harshpatni81@gmail.com>
2026-01-11 14:01:38 +0000

    D

--- Ques3.java
+++ Ques3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ques3{
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("input the year");
+        System.out.println("input the year"); // KKJ
         int year = scanner.nextInt();
 
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
