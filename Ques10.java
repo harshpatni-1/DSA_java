155c06c7856281f880e2b34397cfab00560b472e
harshpatni-1 <harshpatni81@gmail.com>
2026-01-15 10:47:32 +0000

    on it

--- Ques10.java
+++ Ques10.java
@@ -1,26 +1,9 @@
 import java.util.Scanner;
+public class Ques10
 
-public class Ques10 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public static void main(String[]args){
+Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input: number = ");
-        int number = scanner.nextInt();
-        
-        // Handle negative numbers by taking absolute value
-        number = Math.abs(number);
-        
-        int count = 0;
+    System.out.println("Enter: number = ")
 
-        if (number == 0) {
-            count = 1;
-        } else {
-            while (number > 0) {
-                number /= 10; // Remove the last digit
-                count++;      // Increment digit count
-            }
-        }
-
-        System.out.println("Output: " + count);
-    }
 }
