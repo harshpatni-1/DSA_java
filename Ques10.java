e30c7f2ff1ecc089a8d8f585cd6b0479893e8791
harshpatni-1 <harshpatni81@gmail.com>
2026-01-15 11:18:53 +0000

    10

--- Ques10.java
+++ Ques10.java
@@ -7,12 +7,16 @@ public class Ques10 {
         System.out.println("Enter a number: ");
         int number = scanner.nextInt();
         
-        // Convert the number to String - here are 3 ways:
-        String numberAsString1 = String.valueOf(number);     // Way 1
-      //  String numberAsString2 = Integer.toString(number);   // Way 2
-        // String numberAsString3 = number + "";                // Way 3 (concatenation trick)
+        // Convert the number to String
+        String numberAsString1 = String.valueOf(number);
         
         System.out.println("As String: " + numberAsString1);
+        
+        // Now get the length of the string
+        String name = numberAsString1;
+        int nameLength = name.length();
+        
+        System.out.println("Length: " + nameLength);
         
         scanner.close();
     }
