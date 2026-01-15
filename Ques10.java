22fe8adb373bad03db4f4a7305aba3f85d3d68ad
harshpatni-1 <harshpatni81@gmail.com>
2026-01-15 10:48:49 +0000

    due

--- Ques10.java
+++ Ques10.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
-public class Ques10
+public class Ques10{
 
 public static void main(String[]args){
 Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter: number = ")
+    System.out.println("Enter: number = ");
+    int number = scanner.nextInt();
+}
+// Let's say you have a number
+int age = 25;
+
+// Convert it to String - here are 3 ways:
+String ageAsString1 = String.valueOf(age);        // Way 1
+String ageAsString2 = Integer.toString(age);      // Way 2
+String ageAsString3 = age + "";                   // Way 3 (concatenation trick)
+
+System.out.println(ageAsString1);  // Prints: 25 (but as text)
 
 }
